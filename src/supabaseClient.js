3bc072b9b47182378610f0daf4660cc15189c725
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-01 01:21:14 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL, 
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    // sobrescreve o endpoint de Realtime
+    realtime: {
+      // Se você NÃO tiver TLS e o Realtime escutar na porta 4000, use ws://
+      // Caso tenha TLS (via nginx, por ex), use wss:// no lugar. 
+      url: import.meta.env.VITE_SUPABASE_REALTIME_URL ?? 'ws://bd.dkdevs.com.br:4000/realtime/v1',
+      params: {
+        apikey: import.meta.env.VITE_SUPABASE_ANON_KEY
+      }
+    }
+  }
+)
