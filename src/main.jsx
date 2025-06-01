c877d7ad68bd3fded296883f4addca3cb6a52a3f
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-01 11:01:50 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { connectSocket } from './socket'
+
+// Conecta o socket assim que a aplicação inicia
+connectSocket()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
