4c6bb9bed3c0510eedcf8c8a07503b14587cdbb4
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-03 00:13:18 -0300

    Update ChatWindow.jsx

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -3,6 +3,22 @@ import { socket, connectSocket } from '../services/socket'
 import SendMessageForm from './SendMessageForm'
 import { supabase } from '../services/supabaseClient'
 
+    const getFileIcon = (filename = '') => {
+  const ext = filename.split('.').pop().toLowerCase();
+
+  const icons = {
+    pdf: 'https://cdn-icons-png.flaticon.com/512/337/337946.png',
+    doc: 'https://cdn-icons-png.flaticon.com/512/337/337932.png',
+    docx: 'https://cdn-icons-png.flaticon.com/512/337/337932.png',
+    xls: 'https://cdn-icons-png.flaticon.com/512/337/337959.png',
+    xlsx: 'https://cdn-icons-png.flaticon.com/512/337/337959.png',
+    ppt: 'https://cdn-icons-png.flaticon.com/512/337/337953.png',
+    pptx: 'https://cdn-icons-png.flaticon.com/512/337/337953.png',
+  };
+
+  return icons[ext] || 'https://cdn-icons-png.flaticon.com/512/136/136539.png'; // genérico
+};
+
 export default function ChatWindow({ userIdSelecionado }) {
   const [messages, setMessages] = useState([])
   const [isLoading, setIsLoading] = useState(false)
@@ -68,23 +84,6 @@ export default function ChatWindow({ userIdSelecionado }) {
         return [...prev, novaMsg].sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))
       })
     }
-
-    const getFileIcon = (filename = '') => {
-  const ext = filename.split('.').pop().toLowerCase();
-
-  const icons = {
-    pdf: 'https://cdn-icons-png.flaticon.com/512/337/337946.png',
-    doc: 'https://cdn-icons-png.flaticon.com/512/337/337932.png',
-    docx: 'https://cdn-icons-png.flaticon.com/512/337/337932.png',
-    xls: 'https://cdn-icons-png.flaticon.com/512/337/337959.png',
-    xlsx: 'https://cdn-icons-png.flaticon.com/512/337/337959.png',
-    ppt: 'https://cdn-icons-png.flaticon.com/512/337/337953.png',
-    pptx: 'https://cdn-icons-png.flaticon.com/512/337/337953.png',
-  };
-
-  return icons[ext] || 'https://cdn-icons-png.flaticon.com/512/136/136539.png'; // genérico
-};
-
 
     const handleUpdateMessage = (updatedMsg) => {
       const activeUser = currentUserIdRef.current
