15c9884a29a4411bf5d08fc666697f27fc69cac2
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-03 22:04:38 -0300

    Update MessageList.jsx

--- src/components/ChatWindow/MessageList.jsx
+++ src/components/ChatWindow/MessageList.jsx
@@ -1,1 +1,19 @@
 
+// src/components/ChatWindow/MessageList.jsx
+import React from 'react';
+import MessageRow from './MessageRow';
+
+export default function MessageList({ messages, onImageClick, onPdfClick }) {
+  return (
+    <>
+      {messages.map((msg) => (
+        <MessageRow
+          key={msg.id}
+          msg={msg}
+          onImageClick={onImageClick}
+          onPdfClick={onPdfClick}
+        />
+      ))}
+    </>
+  );
+}
