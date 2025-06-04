ac65417ecce49a92edef94838bcffd8a95b9523f
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-03 22:01:50 -0300

    Update ImageMessage.jsx

--- src/components/ChatWindow/messageTypes/ImageMessage.jsx
+++ src/components/ChatWindow/messageTypes/ImageMessage.jsx
@@ -1,1 +1,12 @@
 
+// src/components/ChatWindow/messageTypes/ImageMessage.jsx
+import React from 'react';
+import './ImageMessage.css';
+
+export default function ImageMessage({ url, caption, onClick }) {
+  return (
+    <div className="image-container" onClick={onClick}>
+      <img src={url} alt={caption || 'Imagem'} />
+    </div>
+  );
+}
