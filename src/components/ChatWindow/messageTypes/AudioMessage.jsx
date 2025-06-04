e575bac8be4219c45d09af38ca745e9625df3067
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-03 22:01:08 -0300

    Update AudioMessage.jsx

--- src/components/ChatWindow/messageTypes/AudioMessage.jsx
+++ src/components/ChatWindow/messageTypes/AudioMessage.jsx
@@ -1,1 +1,15 @@
 
+// src/components/ChatWindow/messageTypes/AudioMessage.jsx
+import React from 'react';
+import './AudioMessage.css';
+
+export default function AudioMessage({ url }) {
+  return (
+    <div className="audio-container">
+      <audio controls>
+        <source src={url} type="audio/ogg" />
+        Seu navegador não suporta áudio.
+      </audio>
+    </div>
+  );
+}
