c0ee93e3194875b0dcccce04be511e55214e96b5
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-03 22:02:18 -0300

    Update DocumentMessage.jsx

--- src/components/ChatWindow/messageTypes/DocumentMessage.jsx
+++ src/components/ChatWindow/messageTypes/DocumentMessage.jsx
@@ -1,1 +1,17 @@
 
+// src/components/ChatWindow/messageTypes/DocumentMessage.jsx
+import React from 'react';
+import { getFileIcon } from '../utils/getFileIcon';
+import './DocumentMessage.css';
+
+export default function DocumentMessage({ filename, url, caption, onClick }) {
+  return (
+    <div className="document-container" onClick={onClick}>
+      <img src={getFileIcon(filename)} alt="Ícone de arquivo" />
+      <div className="document-details">
+        <strong>{filename}</strong>
+        {caption && <span>{caption}</span>}
+      </div>
+    </div>
+  );
+}
