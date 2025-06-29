a03f4e49d25862eb2afbe6c3d0e1b2c72b3a9436
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-06-28 23:21:03 -0300

    Update useClickOutside.js

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 export function useClickOutside(refs, callback) {
   useEffect(() => {
     const handleClickOutside = (event) => {
-      const isOutside = Array.isArray(refs)
-        ? refs.every(ref => ref.current && !ref.current.contains(event.target))
-        : refs.current && !refs.current.contains(event.target);
+      const activeRefs = Array.isArray(refs) ? refs.filter(ref => ref.current) : [refs];
+
+      const isOutside = activeRefs.every(ref => !ref.current.contains(event.target));
 
       if (isOutside) callback();
     };
